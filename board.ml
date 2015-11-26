125b31547ed977bdb44bd9548a8aab8108271f5b
Megan <mat333@cornell.edu>
2015-11-25 22:10:26 -0500

    Board is done

--- board.ml
+++ board.ml
@@ -7,6 +7,8 @@ type columns = cell list list
 
 type grid = rows * columns
 
+type direction = Down | Across
+
 
 let safe_hd (lst: 'a list) : 'a option =
   match lst with
@@ -36,13 +38,12 @@ let rec transpose (mat: 'a list list) : 'a list list =
         | Some a -> a) in
     (x::(List.map get_hd xss)) :: transpose (xs::(List.map get_tl xss))
 
-
-
 let init_board () =
   let tw = {letter_mult = 1; word_mult = 3; letter = None} in
   let dw = {letter_mult = 1; word_mult = 2; letter = None} in
   let tl = {letter_mult = 3; word_mult = 1; letter = None} in
   let dl = {letter_mult = 2; word_mult = 1; letter = None} in
+  let ct = {letter_mult = 1; word_mult = 2; letter = Some '@'} in
   let xx = {letter_mult = 1; word_mult = 1; letter = None} in
   let row1 = [tw;xx;xx;dl;xx;xx;xx;tw;xx;xx;xx;dl;xx;xx;tw] in
   let row2 = [xx;dw;xx;xx;xx;tl;xx;xx;xx;tl;xx;xx;xx;dl;xx] in
@@ -51,18 +52,91 @@ let init_board () =
   let row5 = [xx;xx;xx;xx;dw;xx;xx;xx;xx;xx;dw;xx;xx;xx;xx] in
   let row6 = [xx;tl;xx;xx;xx;tl;xx;xx;xx;tl;xx;xx;xx;tl;xx] in
   let row7 = [xx;xx;dl;xx;xx;xx;dl;xx;dl;xx;xx;xx;dl;xx;xx] in
-  let row8 = [tw;xx;xx;dl;xx;xx;xx;xx;xx;xx;xx;dl;xx;xx;tw] in
+  let row8 = [tw;xx;xx;dl;xx;xx;xx;ct;xx;xx;xx;dl;xx;xx;tw] in
   let rows =
     [row1;row2;row3;row4;row5;row6;row7;row8;row7;row6;row5;row4;row3;row2;row1]
   in
   (rows, transpose rows)
 (*   failwith "*drake and josh voice* Megan..." *)
 
+(* Code modeled after OCaml FAQ
+ http://caml.inria.fr/pub/old_caml_site/FAQ/FAQ_EXPERT-eng.html#strings*)
+let to_char_list s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
+
+let rec edit_list lst n word =
+(*   let wlist = to_char_list word in *)
+  match lst with
+  | [] -> []
+  | x::xs -> if n = 0 then
+    (match safe_hd word, safe_tl word  with
+        | None, _ -> x::edit_list xs (n-1) word
+        | _, None -> failwith "empty wordlist"
+        | Some hd , Some tl -> print_string "I'm creating a new letter\n";
+        let new_x = {letter_mult = 1; word_mult = 1; letter = Some hd} in
+              new_x::edit_list xs n tl)
+      else x::(edit_list xs (n-1) word)
+
+let rec edit_index lst m n word =
+  match lst with
+  | [] -> []
+  | x::xs -> (if n = 0 then edit_list x m word else x)::edit_index xs m (n-1) word
+
 let update_board grid word coordinates direction =
-  failwith "todo"
+  let (rows, cols) = grid in
+  let (a,b) = coordinates in
+  let (a',b') = ((int_of_char a)-65, b-1) in
+  let wlist = to_char_list word in
+  match direction with
+  | Down -> let new_cols = edit_index cols a' b' wlist in
+        (transpose new_cols, new_cols)
+  | Across -> let new_rows = edit_index rows b' a' wlist in
+        (new_rows, transpose new_rows)
+
+
+
+let get_type (c: cell) : string =
+  match c.letter_mult, c.word_mult, c.letter with
+  | 1, 3, None -> "| TW "
+  | 1, 2, None -> "| DW "
+  | 3, 1, None -> "| TL "
+  | 2, 1, None -> "| DL "
+  | 1, 1, None -> "|    "
+  | 1, 2, Some a -> "| " ^ (Char.escaped a) ^ "  "
+  | 1, 1, Some a -> "| "^ (Char.escaped a) ^"  "
+  | _, _, _ -> failwith "Not valid tile"
+
+
+
+let rec build_row (row: cell list) : string =
+  match row with
+  | [] -> "|"
+  | x::xs -> (get_type x) ^ (build_row xs)
+
+let rec by_row (rows: cell list list) (n:int) : string =
+  let underline = ("\n    +----+----+----+----+----+----+----+----+----+----+" ^
+    "----+----+----+----+----+\n") in
+  let endline = ("\n    +====+====+====+====+====+====+====+====+====+====+====+" ^
+      "====+====+====+====+\n") in
+  match rows with
+  | [] -> ""
+  | x::[] -> " " ^ (string_of_int n) ^ (if n < 10 then " " else "") ^  " " ^
+      (build_row x) ^ endline
+  | x::xs -> " " ^ (string_of_int n) ^ (if n < 10 then " " else "") ^ " " ^
+      (build_row x) ^ underline ^ (by_row xs (n+1))
+
 
 let print_board grid =
-  failwith "Bales is too quiet"
+  let border = ("\n      A    B    C    D    E    F    G    H    I    J    K    L "^
+    "   M    N    O\n    +====+====+====+====+====+====+====+====+====+====+====+"^
+    "====+====+====+====+\n") in
+  let (rows,cols) = grid in
+  let scrabble = by_row rows 1 in
+  print_string (border ^ scrabble)
+
+
 
 
 
--- board.mli
+++ board.mli
@@ -2,6 +2,7 @@ type rows
 type columns
 type cell
 type grid
+type direction
 
 
 (* Creates board with cells that only contain respective multipliers *)
@@ -9,8 +10,7 @@ val init_board : unit ->  rows * columns
 
 (* Updates the board to include the player's move of word string starting
  * at index (int, int) in direction string. Returns updated board option *)
-val update_board : grid -> string -> int * int -> string ->
-  rows * columns option
+val update_board : grid -> string -> char * int -> direction -> grid
 
 (* Prints the current board *)
 val print_board : grid -> unit
