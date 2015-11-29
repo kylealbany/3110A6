c788e8caa416a9cb0c54ae9d4aecdde63926fc18
kylealbany <kylealbany@gmail.com>
2015-11-29 00:00:44 -0500

    forgot to add dict.ml in previous commit

---
+++ dict.ml
@@ -0,0 +1,17 @@
+open RadixTree.PString
+
+type key = string
+type value = string
+type dict = string t
+
+
+let empty = MapChar.empty
+
+let lookup dict key =
+  failwith "unimplemented"
+
+let member dict key =
+  failwith "unimplemented"
+
+let insert dict key value =
+  failwith "unimplemented"
