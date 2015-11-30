8b5f39ae5de8d3b72afb6bad31fc2de2523dbb1e
balesandrew <apb224@cornell.edu>
2015-11-30 01:30:30 -0500

    permutation working

--- ai.ml
+++ ai.ml
@@ -1,5 +1,3 @@
-open Board
-open Gamestate
 
 (* distribute and permutation modified from
  * http://www.dietabaiamonte.info/79762.html#sthash.QgjGV9wd.dpuf
@@ -23,13 +21,29 @@ let distribute c l =
       helper subs (ch::a)
   in helper s [] *)
 
-(* accepts a byte list and returns a (byte list) list containing the permutations *)
-  let rec permutation = function
+(* convert a bytes list to a string *)
+let blist_to_string b =
+  let rec helper b str =
+    match b with
+    | [] -> str
+    | hd::tl -> let new_s = String.make 1 hd in
+                let new_str = String.concat "" (new_s::[str]) in
+                  helper tl new_str
+  in helper b ""
+
+(* accepts a byte list and returns a string list containing the permutations *)
+let permutation s =
+  let rec perm s =
+    match s with
     | [] -> [[]]
     | hd::tl -> List.fold_left (fun acc x -> List.rev_append (distribute hd x) acc)
-      [] (permutation tl)
+      [] (perm tl)
+  in
+  let blist_perm = perm s in
+    List.map blist_to_string blist_perm
 
-let gen_word_list game = failwith "un"
+
+let gen_word_list game = failwith "unimplimented"
 (*   let tiles = game.tiles in
   let perms = permutation tiles in *)
 
@@ -43,6 +57,4 @@ let should_ex movelist =
 let exchange_tiles game =
   failwith "JK Kyle's actually a pretty cool guy"
 
-
-let perm = permutation ['a';'b';'c']
 
